d599feecd0d949973e58ef9f555dc4a68edfdcf8
firasel <mdrasel601390@gmail.com>
2021-05-30 15:50:02 +0600

    complete the header-section and footer-section md-rasel

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
-
 import AboutUs from '../AboutUs/AboutUs';
 import ExploreByCities from '../ExploreByCities/ExploreByCities';
-import FeaturedProperties from '../FeaturedProperties/FeaturedProperties'
+import FeaturedProperties from '../FeaturedProperties/FeaturedProperties';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
 
 const Home = () => {
     return (
         <>
-         
+         <Header></Header>
          <AboutUs></AboutUs>
          <FeaturedProperties></FeaturedProperties>
          <ExploreByCities></ExploreByCities>
-         
-         
+         <Footer></Footer>
         </>
     );
 };
