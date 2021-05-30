e1489c6fb2662cf292989927fc95b246fd8a5df5
silviaplabon <silviaplabon@gmail.com>
2021-05-30 11:17:38 +0600

    silvia: Added Featured Apartments with FeaturedDetail page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import AboutUs from '../AboutUs/AboutUs';
 import ExploreByCities from '../ExploreByCities/ExploreByCities';
-
+import FeaturedProperties from '../FeaturedProperties/FeaturedProperties'
 
 const Home = () => {
     return (
         <>
          
          <AboutUs></AboutUs>
+         <FeaturedProperties></FeaturedProperties>
          <ExploreByCities></ExploreByCities>
+         
          
         </>
     );
