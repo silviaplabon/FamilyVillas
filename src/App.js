1a373bf4eb8df325ccaffefc0d60a0f5ecb48094
ShahbajKhan <shktoky@gmail.com>
2021-05-30 10:42:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+import Home from "./components/Home/Home/Home";
+import NoMatch from "./components/Home/NoMatch/NoMatch";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="*">
+            <NoMatch></NoMatch>
+         </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import AboutUs from '../AboutUs/AboutUs';
+import ExploreByCities from '../ExploreByCities/ExploreByCities';
+
+
+const Home = () => {
+    return (
+        <>
+         
+         <AboutUs></AboutUs>
+         <ExploreByCities></ExploreByCities>
+         
+        </>
+    );
+};
+
+export default Home;
